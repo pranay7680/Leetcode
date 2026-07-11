af5feb815313f02cbc1c2ebef1dce059062c3598
THODUPUNURI PRANAY KUMAR <t.pranaykumar768@gmail.com>
2026-07-12 00:50:40 +0530

    Time: 3 ms (87.04%), Space: 43.3 MB (41.31%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,21 +1,12 @@
 class Solution {
     public boolean isValid(String s) {
-        ArrayDeque<Character> stack = new ArrayDeque<>();
-
-        for (char ch : s.toCharArray()) {
-            if (ch == '(' || ch == '{' || ch == '[') {
-                stack.push(ch);
-            } else {
-                if (stack.isEmpty()) return false;
-
-                char top = stack.pop();
-
-                if ((ch == ')' && top != '(') ||
-                    (ch == '}' && top != '{') ||
-                    (ch == ']' && top != '[')) {
-                    return false;
-                }
-            }
+        Stack<Character> stack = new Stack<>();
+        for (char c:s.toCharArray()) {
+            if (c=='(') stack.push(')');
+            else if(c=='{') stack.push('}');
+            else if(c=='[') stack.push(']');
+            else if(stack.isEmpty() || stack.pop()!= c)
+                return false;
         }
 
         return stack.isEmpty();
