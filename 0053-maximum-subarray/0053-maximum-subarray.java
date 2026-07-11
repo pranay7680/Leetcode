5517260cfe3262440853737fc35b9ba1eaa638f6
THODUPUNURI PRANAY KUMAR <t.pranaykumar768@gmail.com>
2026-07-11 23:55:07 +0530

    Time: 1 ms (99.96%), Space: 77.1 MB (82.37%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -4,7 +4,7 @@ class Solution {
         int max=Integer.MIN_VALUE;
         for(int i=0;i<nums.length;i++){
             sum+=nums[i];
-            max=Math.max(sum,max);
+            max=max>sum?max:sum;
             sum=(sum>0)?sum:0;
         }
         return max;
