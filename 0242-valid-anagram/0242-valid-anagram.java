e28f5907907d09d11e2db3a1763958ce2b14e903
THODUPUNURI PRANAY KUMAR <t.pranaykumar768@gmail.com>
2026-07-12 00:52:13 +0530

    Time: 2 ms (99.61%), Space: 44.8 MB (55.07%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,12 +1,19 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()){
-            return false;
+        int[] arr=new int[26];
+
+        for(char ch:s.toCharArray()){
+            arr[ch-'a']++;
         }
-        char a[]=s.toCharArray();
-        char b[]=t.toCharArray();
-        Arrays.sort(a);
-        Arrays.sort(b);
-        return new String(a).equals(new String(b));
+        for(char ch:t.toCharArray()){
+            arr[ch-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(arr[i]!=0){
+                return false;
+            }
+        }
+        return true;
+
     }
 }
