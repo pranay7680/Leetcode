6e729a659b05fd56c3e3fefe210d8a3360bcbafb
THODUPUNURI PRANAY KUMAR <t.pranaykumar768@gmail.com>
2026-07-12 12:24:15 +0530

    Time: 4 ms (100%), Space: 45.9 MB (72.46%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,12 +1,14 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0 || (x % 10 == 0 && x != 0))
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
-        int rev=0,y=x;
-        while(x>0){
-            rev=rev*10+(x%10);
-            x=x/10;
         }
-        return (rev==y)?true:false;
+
+        int reversed = 0;
+        while (x > reversed) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+        return x == reversed || x == reversed / 10;
     }
 }
