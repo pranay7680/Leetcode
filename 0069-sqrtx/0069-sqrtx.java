aefb00cc4422c447cfe5ac1577a8fd2f1b4bf6d2
THODUPUNURI PRANAY KUMAR <t.pranaykumar768@gmail.com>
2026-07-15 22:10:24 +0530

    Time: 1 ms (97.62%), Space: 42.7 MB (42.92%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x<2){
+            return x;
+        }
+        int l=1,r=x/2;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if((long)mid*mid==x)
+                return mid;
+            else if((long)mid*mid<x)
+                l=mid+1;
+            else
+                r=mid-1;
+            
+        }
+        return r;
+    }
+}
