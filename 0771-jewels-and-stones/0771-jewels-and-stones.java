ec0233fc6bd4691ba3f00cdb1f45c970787e8001
THODUPUNURI PRANAY KUMAR <t.pranaykumar768@gmail.com>
2026-07-16 18:21:44 +0530

    Time: 0 ms (100%), Space: 43.1 MB (72.7%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -2,7 +2,7 @@ class Solution {
     public int numJewelsInStones(String jewels, String stones) {
         int c=0;
         for(int i=0;i<stones.length();i++){
-            if(jewels.contains(""+stones.charAt(i))){
+            if(jewels.indexOf(stones.charAt(i))!=-1){
                 c++;
             }
         }
