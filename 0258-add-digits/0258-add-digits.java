83c00adadd67df3231de77495ce686863e743cc5
THODUPUNURI PRANAY KUMAR <t.pranaykumar768@gmail.com>
2026-07-19 14:13:00 +0530

    Time: 1 ms (97.93%), Space: 42.5 MB (80.29%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,14 +1,5 @@
 class Solution {
     public int addDigits(int num) {
-        int sum;
-        while(num>9){
-            sum=0;
-            while(num>0){
-            sum+=num%10;
-            num=num/10;
-            }
-            num=sum;
-        }
-        return num;
+        return(num==0)?0:(1+(num-1)%9);
     }
 }
