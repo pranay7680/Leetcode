6dfeddf009c2bf57567a69c051c3233e6a7d7104
THODUPUNURI PRANAY KUMAR <t.pranaykumar768@gmail.com>
2026-07-25 00:34:45 +0530

    Time: 1 ms (100%), Space: 43.6 MB (90.9%) - LeetHub

--- 3280-convert-date-to-binary/3280-convert-date-to-binary.java
+++ 3280-convert-date-to-binary/3280-convert-date-to-binary.java
@@ -1,19 +1,36 @@
 class Solution {
     public String convertDateToBinary(String date) {
-        String [] s = date.split("-");
-        String res[] =new String[s.length];
-        int j=0;
-        for(String i:s){
-            int n = Integer.parseInt(i);
-            StringBuilder bb = new StringBuilder();
-            while(n>0){
-                bb.append(n%2);
-                n = n/2;
+        // String [] s = date.split("-");
+        // String res[] =new String[s.length];
+        // int j=0;
+        // for(String i:s){
+        //     int n = Integer.parseInt(i);
+        //     StringBuilder bb = new StringBuilder();
+        //     while(n>0){
+        //         bb.append(n%2);
+        //         n = n/2;
+        //     }
+        //     bb.reverse();
+        //     res[j]=bb.toString();
+        //     j++;
+        // }
+        // return String.join("-",res);
+        StringBuilder ans = new StringBuilder();
+        int sum=0;
+        for(int i=0;i<date.length();i++){
+            char ch = date.charAt(i);
+            if(ch=='-'){
+                ans.append(Integer.toBinaryString(sum));
+                ans.append('-');
+                sum=0;
             }
-            bb.reverse();
-            res[j]=bb.toString();
-            j++;
+            else{
+                sum = sum*10+ ch-'0';
+            }
+            if(i==date.length()-1){
+                ans.append(Integer.toBinaryString(sum));
+            }
         }
-        return String.join("-",res);
+        return ans.toString();
     }
 }
