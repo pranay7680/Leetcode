8158b1bf92feeec96db11a63471893561a1eb43b
THODUPUNURI PRANAY KUMAR <t.pranaykumar768@gmail.com>
2026-07-25 01:00:38 +0530

    Time: 1 ms (99.12%), Space: 42.4 MB (84.8%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.java
+++ 3945-digit-frequency-score/3945-digit-frequency-score.java
@@ -2,7 +2,8 @@ class Solution {
     public int digitFrequencyScore(int n) {
         int sum=0;
         while(n>0){
-            sum += n%10;
+             int rem=n%10;
+            sum += rem;
             n = n/10;
         }
         return sum;
