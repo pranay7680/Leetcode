adbea940d1e98e0ac3947d2be5ba497a291873ce
THODUPUNURI PRANAY KUMAR <t.pranaykumar768@gmail.com>
2026-07-25 00:01:15 +0530

    Time: 1 ms (98.64%), Space: 43.3 MB (54.29%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.java
+++ 3110-score-of-a-string/3110-score-of-a-string.java
@@ -2,9 +2,7 @@ class Solution {
     public int scoreOfString(String s) {
         int sum=0;
         for(int j=1;j<s.length();j++){
-            char a = s.charAt(j);
-            char b = s.charAt(j-1);
-            sum = sum + Math.abs(a-b);
+            sum += Math.abs(s.charAt(j)-s.charAt(j-1));
         }
         return sum;
     }
